41c4b354fa699f2074a33d59230a9c934e2ecbe0
Patrick O'Grady <prohb125@gmail.com>
2019-12-28 09:42:59 -0600

    Use base58 encoding of hash for tests

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/patrick-ogrady/merklep2p
 go 1.13
 
 require (
+	github.com/btcsuite/btcutil v0.0.0-20191219182022-e17c9730c422
 	github.com/stretchr/testify v1.4.0
 	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
--- node_test.go
+++ node_test.go
@@ -3,19 +3,18 @@ package merklep2p
 import (
 	"testing"
 
+	"github.com/btcsuite/btcutil/base58"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCalculateHash(t *testing.T) {
 	n := Node{
-		Children: [][]byte{
-			[]byte("left"),
-			[]byte("right"),
-		},
+		Left:  []byte("left"),
+		Right: []byte("right"),
 	}
 
 	hash, err := n.CalculateHash()
 
 	assert.NoError(t, err)
-	assert.Equal(t, []byte("sicker"), hash)
+	assert.Equal(t, "3VNzeWnMPADdMM2fcfrQxWex2nHupwvvGA1egGwaSKFM", base58.Encode(hash))
 }
